8f62985e9965abe5901dfc2efbefb9f0c7547a9e
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2021-12-12 10:42:29 +0530

    2WD nodemcu code

--- Hand Module/Hand_module_NodeMCU_code/Hand_module_NodeMCU_code.ino
+++ Hand Module/Hand_module_NodeMCU_code/Hand_module_NodeMCU_code.ino
@@ -1,6 +1,19 @@
 #include <SPI.h>                             //Library for SPI 
+#include <ESP8266WiFi.h>
+
 
 int x;
+
+byte ledPin = 2;
+char ssid[] = "paya1";
+char pass[] = "billu12345";
+WiFiServer server(80);
+
+IPAddress ip(192, 168, 29, 83);            // IP address of the server
+IPAddress gateway(192, 168, 29, 1);           // gateway of your network
+IPAddress subnet(255, 255, 255, 0);          // subnet mask of your network
+
+
 void setup(void) {
     Serial.begin(115200);                   //Starts Serial Communication at Baud Rate 115200 
 
@@ -8,6 +21,15 @@ void setup(void) {
     SPI.begin();                            //Begins the SPI commnuication
     SPI.setClockDivider(SPI_CLOCK_DIV8);    //Sets clock for SPI communication at 8 (16/8=2Mhz)
     digitalWrite(SS, HIGH);                  // Setting SlaveSelect as HIGH (So master doesnt connnect with slave)
+
+    WiFi.config(ip, gateway, subnet);
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    server.begin();
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop(void)
@@ -18,7 +40,23 @@ void loop(void)
     Mastersend = x;
     Mastereceive = SPI.transfer(Mastersend); //Send the mastersend value to slave also receives value from slave
 
-    Serial.println(Mastereceive);
+    //Serial.println(Mastereceive);
 
-    delay(100);
+    //delay(100);
+
+    WiFiClient client = server.available();
+    if (client) {
+        if (client.connected()) {
+            digitalWrite(ledPin, LOW);
+            Serial.println(".");
+            String request = client.readStringUntil('\r');
+            //Serial.print("From client: ");
+            //Serial.print(request);
+            client.flush();
+            //client.println("Hi client! No, I am listening.\r");
+            client.println(Mastereceive);
+            digitalWrite(ledPin, HIGH);
+        }
+        client.stop();
+    }
 }
