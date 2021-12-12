3eab8ba2cc1b43cd6b20ee4d0ad2a91803848b90
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2021-12-12 09:29:52 +0530

    Hand module Nodemcu code started

---
+++ Hand Module/Hand_module_NodeMCU_code/Hand_module_NodeMCU_code.ino
@@ -0,0 +1,24 @@
+#include <SPI.h>                             //Library for SPI 
+
+int x;
+void setup(void) {
+    Serial.begin(115200);                   //Starts Serial Communication at Baud Rate 115200 
+
+
+    SPI.begin();                            //Begins the SPI commnuication
+    SPI.setClockDivider(SPI_CLOCK_DIV8);    //Sets clock for SPI communication at 8 (16/8=2Mhz)
+    digitalWrite(SS, HIGH);                  // Setting SlaveSelect as HIGH (So master doesnt connnect with slave)
+}
+
+void loop(void)
+{
+    int Mastersend;
+    float Mastereceive;
+
+    Mastersend = x;
+    Mastereceive = SPI.transfer(Mastersend); //Send the mastersend value to slave also receives value from slave
+
+    Serial.println(Mastereceive);
+
+    delay(100);
+}
